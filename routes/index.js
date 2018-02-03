84f69d5b43f56b304458066c04362f43695264ed
aditya-9034 <sharmadity123.as@outlook.com>
2018-02-03 17:03:16 +0000

    created user profile

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express=require("express");
 var router=express.Router();
 var passport=require("passport");
 var User=require("../models/user");
+var Campground=require("../models/campground");
 
 router.get("/",function(req,res){
    res.render("landing"); 
@@ -15,7 +16,14 @@ router.get("/register",function(req,res){
 
 //handle signup logic
 router.post("/register",function(req,res){
-    var newUser=new User({username:req.body.username});
+    var newUser=new User({
+        username:req.body.username,
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        email:req.body.email,
+        avatar:req.body.avatar
+    });
+    
     if(req.body.adminCode === "secretcode123"){
         newUser.isAdmin=true;
     }
@@ -25,8 +33,8 @@ router.post("/register",function(req,res){
             return res.render("register",{error:err.message});
         }
         passport.authenticate("local")(req,res,function(){
-           req.flash("success","Welcome to YelpCamp "+user.username);
-           res.redirect("/campgrounds") 
+          req.flash("success","Welcome to YelpCamp "+user.username);
+          res.redirect("/campgrounds") 
         });
     });
   
@@ -52,5 +60,20 @@ router.get("/logout",function(req,res){
     res.redirect("/campgrounds");
 });
 
+router.get("/users/:id",function(req,res){
+    User.findById(req.params.id,function(err,foundUser){
+       if(err){
+           req.flash("error","Something went wrong");
+           res.redirect("/");
+       } 
+       Campground.find().where('author.id').equals(foundUser._id).exec(function(err,campgrounds){
+           if(err){
+               req.flash("error","Something went wrong");
+               res.redirect("/");
+           }
+           res.render("users/show",{user:foundUser,campgrounds:campgrounds});
+       })
+    });
+});
 
 module.exports=router;
